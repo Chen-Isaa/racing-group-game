befc60ce69d41681e0c2d396f0a923118878901f
Chen-Isaa <124813197+Chen-Isaa@users.noreply.github.com>
2023-02-10 10:09:41 -0800

    added bleachers

--- Racing Game/Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
+++ Racing Game/Assets/PROMETEO - Car Controller/Scripts/CameraFollow.cs
@@ -2,34 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour {
+public class scripy : MonoBehaviour
+{
+    public Transform cameraTarget;
+    public float sSpeed = 10.0f;
+    public Vector3 dist;
+    public Transform lookTarget;
 
-	public Transform carTransform;
-	[Range(1, 10)]
-	public float followSpeed = 2;
-	[Range(1, 10)]
-	public float lookSpeed = 5;
-	Vector3 initialCameraPosition;
-	Vector3 initialCarPosition;
-	Vector3 absoluteInitCameraPosition;
-
-	void Start(){
-		initialCameraPosition = gameObject.transform.position;
-		initialCarPosition = carTransform.position;
-		absoluteInitCameraPosition = initialCameraPosition - initialCarPosition;
-	}
-
-	void FixedUpdate()
-	{
-		//Look at car
-		Vector3 _lookDirection = (new Vector3(carTransform.position.x, carTransform.position.y, carTransform.position.z)) - transform.position;
-		Quaternion _rot = Quaternion.LookRotation(_lookDirection, Vector3.up);
-		transform.rotation = Quaternion.Lerp(transform.rotation, _rot, lookSpeed * Time.deltaTime);
-
-		//Move to car
-		Vector3 _targetPos = absoluteInitCameraPosition + carTransform.transform.position;
-		transform.position = Vector3.Lerp(transform.position, _targetPos, followSpeed * Time.deltaTime);
-
-	}
-
+    void FixedUpdate()
+    {
+        Vector3 dPos = cameraTarget.position + dist;
+        Vector3 sPos = Vector3.Lerp(transform.position, dPos, sSpeed * Time.deltaTime);
+        transform.position = sPos;
+        transform.LookAt(lookTarget.position);
+    }
 }
